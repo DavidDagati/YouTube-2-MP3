054c6f2d7fabe12649481289837f5eace554f862
David Dagati <ddagati@hotmail.ca>
2022-11-05 18:21:32 -0400

    Downloaded folder, research into failing .mp4s

--- convert.py
+++ convert.py
@@ -1,25 +1,6 @@
 import youtube_dl
 
-
-def to_mp3(video_info):
-
-    # yn = input(f"Is {video_info['title']} a Suitable Name? (Y/N): ")
-
-    # if yn == "Y":
-    #     filename = f"{video_info['title']}.mp3"
-    # else:
-    #     name= input("Enter your prefered filename: ")
-    #     query= r"^[ .]|[/<>:\"\\|?*]+|[ .]$"
-    #     illegal_char= re.findall(query, name)
-
-    #     if len(illegal_char) == 0:
-    #         return
-    #     else:
-    #         name= re.sub(query, "_", name)
-
-    #     filename= name + ".mp3"
-
-    filename = f"{video_info['title']}.mp3"
+def to_mp3(filename, video_info):
         
     options= {
                 'format': 'bestaudio/best',
@@ -32,21 +13,15 @@ def to_mp3(video_info):
 
     print(f"{filename} Download Complete!")
 
-def to_mp4(video_info):
-
-    filename = f"{video_info['title']}.mp4"
+def to_mp4(filename, video_info):
         
     options= {
-                'format': 'bestvideo[ext=mp4]',
+                'format': 248,
                 'keepvideo': False,
                 'outtmpl': filename,
             }
 
-
-
     with youtube_dl.YoutubeDL(options) as ydl:
         ydl.download([video_info['webpage_url']])
-
-    
 
     print(f"{filename} Download Complete!")    
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import youtube_dl
+from pathvalidate import sanitize_filepath
 from convert import to_mp3, to_mp4
 
 def select_output():
@@ -7,17 +8,38 @@ def select_output():
     output = input("Would you like to download an MP3 or an MP4?: ")
     return output
 
+def check_directory():
+
+    current = os.getcwd()
+    search = "\\Downloaded"
+
+    if not os.path.isdir(current + search):
+        os.mkdir(current + search)
+
+def select_filename():
+
+    filename = input("What would you like to name your file?: ")
+    clean_filename = sanitize_filepath(filename)
+
+    return clean_filename
+
 
 if __name__=='__main__':
 
-    # if ''
+    check_directory()
 
     video_url= input("Enter YouTube URL: ")
     video_info= youtube_dl.YoutubeDL().extract_info(url= video_url,download= False)
 
+    # for i, format in enumerate(video_info['formats']):
+    #    print(f"[{i}] {format['format']}")
+
     filetype = select_output()
+    name = select_filename()
 
     if filetype == "MP3" or "mp3":
-        to_mp3(video_info)
+        filename = name + ".mp3"
+        to_mp3(filename, video_info)
     elif filetype == "MP4" or "mp4":
-        to_mp4(video_info)
+        filename = name + "mp4"
+        to_mp4(filename, video_info)
